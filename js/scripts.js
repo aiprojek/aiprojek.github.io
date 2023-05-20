be9e2d0124b41ec07e3b596db36112ee688c0333
aiprojek <71024676+aiprojek@users.noreply.github.com>
2023-05-21 06:41:29 +0700

    Add files via upload

---
+++ js/scripts.js
@@ -0,0 +1,7 @@
+/*!
+* Start Bootstrap - Personal v1.0.1 (https://startbootstrap.com/template-overviews/personal)
+* Copyright 2013-2023 Start Bootstrap
+* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-personal/blob/master/LICENSE)
+*/
+// Get current year
+document.getElementById("year").innerHTML = new Date().getFullYear();
